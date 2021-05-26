18f6e89674e7824bde6cf4bc5e1c9b71e10f0888
yonghwankim.dev <yonghwankim.dev@gmail.com>
2021-05-26 21:09:17 +0900

    first sample tree

---
+++ Tree/practice/BinaryTree/set1/BinaryTree.java
@@ -0,0 +1,18 @@
+package Tree.practice.BinaryTree.set1;
+
+// A Java program to introduce Binary Tree
+public class BinaryTree {
+	// Root of Binary Tree
+	Node root;
+	
+	// Constructors
+	public BinaryTree(int key) {
+		root = new Node(key);
+	}
+	
+	public BinaryTree()
+	{
+		root = null;
+	}
+	
+}
---
+++ Tree/practice/BinaryTree/set1/Node.java
@@ -0,0 +1,14 @@
+package Tree.practice.BinaryTree.set1;
+
+/* Class containing left and right child of current
+ * node and key value */
+public class Node {
+	int key;
+	Node left, right;
+	public Node(int item) {
+		this.key = item;
+		left = right = null;
+	}
+}
+
+
