f59ad5342f9278f787823fc4e49f187d0665cd38
yonghwankim.dev <yonghwankim.dev@gmail.com>
2021-12-10 17:35:11 +0900

    add/remove method

--- LinkedList/Implement/LinkedList.java
+++ LinkedList/Implement/LinkedList.java
@@ -1,176 +1,180 @@
 package LinkedList.Implement;
 
-//연결리스트 삽입 메서드 예제
-public class LinkedList
-{
-	 Node head;  // 리스트의 헤드
+public class LinkedList<E> {
+	class Node<E>{
+		E data;
+		Node<E> next;
+		
+		public Node(E data) {
+			this.data = data;
+			this.next = null;
+		}	
+	}
 	
-	 /* 연결리스트를 구성하는 노드 클래스*/
-	 static class Node
-	 {
-	     int data;
-	     Node next;
-	     Node(int d) {data = d; next = null; }
-	 }
+	private Node<E> head;
+	private Node<E> tail;
+	private int currentSize;
 	
-	 /* 연결리스트 맨 앞에 노드 삽입 */
-	 public void push(int new_data)
-	 {
-	     /* 1 & 2: 노드 할당 및 생성자에 데이터 삽입 */
-	     Node new_node = new Node(new_data);
+	public LinkedList(){
+		head = null;
+		tail = null;
+		currentSize = 0;
+	}
 	
-	     /* new_node의 next는 head를 가리킵니다. */
-	     new_node.next = head;
+	public void addFirst(E data) {
+		Node<E> newNode = new Node<E>(data);
+		
+		// 1. 경계조건, 자료구조가 비어있는 경우
+		if(head==null)
+		{
+			tail = newNode;
+		}
+		
+		newNode.next = head;
+		head = newNode;		
+		currentSize++;
+	}
 	
-	     /* head는 newNode를 가리킵니다. */
-	     head = new_node;
-	 }
+	public void addLast(E data) {
+		
+		Node<E> newNode = new Node<E>(data);
+		// 1. 경계조건, 자료구조가 비어있는 경우
+		if(head==null)
+		{
+			head = tail = newNode;
+			currentSize++;
+			return;
+		}
+		
+		tail.next = newNode;		
+		tail = newNode;
+		
+		currentSize++;
+	}
 	
-	 /* 주어진 인덱스 자리에 새로운 노드를 삽입합니다. */
-	 public void insert(int idx, int new_data)
-	 {
-		 Node newNode = new Node(new_data);
-		 
-		 if(idx<0 || idx>size())	// 부적절한 인덱스 범위
-		 {
-			 throw new IndexOutOfBoundsException();
-		 }
-		 else if(idx==0)			// 0번째 위치에 노드를 삽입하고자 하는 경우
-		 {
-			 newNode.next = head;
-			 head = newNode;
-		 }
-		 else
-		 {
-			 Node prev = head;
-			 // 특정 인덱스-1번째 노드로 이동 (prev)
-			 for(int i=0;i<idx-1;i++)
-			 {
-				 prev = prev.next;
-			 }
-			 
-			 // 삽입 처리
-			 newNode.next = prev.next;
-			 prev.next = newNode;
-		 }
-	 }
-	 
-	 /* 연결리스트 마지막에 새로운 노드를 추가합니다.*/
-	 public void append(int new_data)
-	 {
-	     /* 1. 새로운 노드 생성 */
-	     Node new_node = new Node(new_data);
+	public E removeFirst() {
+		// 경계조건 1. Empty, 자료구조가 비어있는 경우
+		if(head==null)
+		{
+			return null;
+		}
+		
+		E delData = head.data;
+		
+		// 경계조건 2. Single Element, 삭제할 노드가 단 하나남은 노드인 경우 
+		// head, tail은 null을 가리키도록 함
+		if(head==tail)
+		{
+			head = tail = null;
+		}
+		else
+		{
+			head = head.next;
+		}
+		
+		currentSize--;
+		
+		return delData;
+	}
 	
-	     /* 2. head의 검사
-	         만약 연결리스트가 비어있다면 head는 새로운 노드를 가리키고 종료합니다.*/
-	     if (head == null)
-	     {
-	         head = new Node(new_data);
-	         return;
-	     }
+	public E removeLast() {
+		// 경계조건 1. Empty, 자료구조가 비어있는 경우
+		if(head==null)
+		{
+			return null;
+		}
+		
+		// 경계조건 2. Single Element, 자료구조에서 요소가 하나 있는 경우
+		if(head==tail)
+		{
+			return removeFirst();
+		}
+		
+		Node<E> current = head;
+		Node<E> previous = null;
+		
+		while(current!=tail)
+		{
+			previous = current;
+			current = current.next;		 
+		}
+		
+		previous.next = null;
+		tail = previous;
+		
+		currentSize--;
+		
+		return current.data;
+		
+	}
 	
-	     /* 3. 새로운 노드는 맨 뒤에 추가하기 때문에 next는 null로 설정합니다.*/
-	     new_node.next = null;
+	public E remove(E obj) {
+		Node<E> current=head, previous=null;
+		
+		// 경계 조건 Empty 포함
+		while(current!=null)
+		{
+			// 찾고자 하는 요소인 경우
+			if(((Comparable<E>)obj).compareTo(current.data)==0)
+			{
+				// 경계조건 Single Element, Beginning
+				if(current==head)
+				{
+					return removeFirst();
+				}
+				// 경계조건 End
+				if(current==tail)
+				{
+					return removeLast();
+				}
+				// 경계조건 Middle 포함
+				previous.next = current.next;
+				currentSize--;
+				return current.data;
+			}
+			// 찾고자 하는 요소가 아닌 경우
+			previous = current;
+			current = current.next;
+		}
 	
-	     /* 4. 연결리스트의 마지막 노드를 찾기 위한 순회를 수행합니다. */
-	     Node last = head;
-	     while (last.next != null)
-	         last = last.next;
+		return null;
+	}
 	
-	     /* 5. 기존 마지막 노드의 next는 새로운 노드(new_node)를 가리킵니다. */
-	     last.next = new_node;
-	     return;
-	 }
-	 
-	 // key가 주어진 경우 해당 key와 동일하고 처음 마주치는 노드를 제거한다
-	 void deleteKeyNode(int key)
-	 {
-		 // cur는 head에서부터 시작함
-		 Node cur = head, prev = null;
-	 
-		 // head가 삭제 대상 노드인 경우
-		 if (cur != null && cur.data == key) 
-		 {
-			 head = cur.next; // head가 삭제되므로 head를 다음 노드로 가리킨다.
-			 return;
-		 }
-	 
-		 // 삭제될 노드(cur)와 그 이전 노드(prev)를 탐색한다.
-		 while (cur != null && cur.data != key) 
-		 {
-			 prev = cur;
-			 cur = cur.next;
-		 }
-	 
-		 // 연결리스트에서 해당 key값을 못찾은 경우 메서드 종료
-		 if (cur == null)
-		 {
-			 return; 
-		 }
-			 
-		 // 연결리스트로부터 연결해제
-		 prev.next = cur.next;	
-	 }
-	 
-	 // 연결리스트의 position 값에 위치한 노드를 삭제
-	 void deletePositionNode(int position)
-	 {	  
-		 // prev는 head를 가리킴
-		 Node prev = head;
-	 
-		// 연결리스트가 비어있는 경우 삭제연산 중지
-		 if (head == null)
-		 {
-			 return; 
-		 }
-		 
-		 // 만약 삭제될 노드가 head(맨앞)인 경우
-		 if (position == 0)
-		 {
-			 head = prev.next;   // head 변경
-			 return;
-		 }
-	 
-		 // 삭제될 노드의 이전 노드(prev)를 탐색
-		 // prev!=null을 하는 이유는 position값이 노드의 개수보다 적을 수 있기 때문
-		 for (int i=0; prev!=null && i<position-1; i++)
-		 {
-			 prev = prev.next;
-		 }
-		 
-		 // 만약 position값이 노드의 개수보다 많은 경우 삭제연산 종료
-		 if (prev == null || prev.next == null)
-		 {
-			 return;
-		 }
-			 
-		 // 이전 노드(prev)는 삭제될 노드의 다음 노드를 가리킨다.
-		 prev.next = prev.next.next;;  // 연결리스트로부터 삭제될 노드 연결 해제 수행
-	 }
+	public boolean contains(E obj)
+	{
+		Node<E> current=head;
+		
+		// 경계 조건 Empty 포함
+		while(current!=null)
+		{
+			// 찾고자 하는 요소인 경우
+			if(((Comparable<E>)obj).compareTo(current.data)==0)
+			{
+				return true;
+			}
+			// 찾고자 하는 요소가 아닌 경우
+			current = current.next;
+		}
+		
+		// 요소가 포함되지 않은 경우
+		return false;
+	}
 	
-	 /* 연결리스트를 head에서부터 순회 */
-	 public void printList()
-	 {
-	     Node tnode = head;
-	     while (tnode != null)
-	     {
-	         System.out.print(tnode.data+" ");
-	         tnode = tnode.next;
-	     }
-	     System.out.println();
-	 }
-	 
-	 /* 연결 리스트의 크기를 반환합니다. */
-	 public int size() {
-		 int count = 0;
-		 Node cur = head;
-		 
-		 while(cur!=null)
-		 {
-			 cur = cur.next;
-			 count++;
-		 }
-		 return count;
-	 }
 
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		Node<E> cur = head;
+		
+		while(cur!=null)
+		{
+			sb.append(cur.data + " ");
+			cur=cur.next;
+		}
+		sb.append("\n");
+		
+		return sb.toString().trim();
+	}
+	
+	
 }
--- LinkedList/Implement/LinkedListTest.java
+++ LinkedList/Implement/LinkedListTest.java
@@ -1,90 +1,65 @@
 package LinkedList.Implement;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 class LinkedListTest {
 
-	@Test 
-	void pushTest(){
-		/* Start with the empty list */
-		LinkedList list = new LinkedList();
-		list.push(1);
-		list.push(2);
-		list.push(3);
+	@Test
+	void addFirstTest() {
+		LinkedList<String> list = new LinkedList<String>();
 		
-		list.printList();	// Expected Output : [3,2,1]
+		list.addFirst("A");
+		list.addFirst("B");
+		list.addFirst("C");
+		
+		System.out.println(list);	// Expected Output : C B A
 	}
 	
 	@Test
-	void insertTest() {
-		/* Start with the empty list */
-		LinkedList list = new LinkedList();
-		list.append(1);
-		list.append(2);
-		list.append(3);
+	void removeFirstTest() {
+		LinkedList<String> list = new LinkedList<String>();
 		
+		list.addFirst("A");
+		list.addFirst("B");
+		list.addFirst("C");
 		
-		
-		System.out.println("연결리스트 insert 메서드 예제");
-		System.out.println("before 연결리스트 : ");
-	    list.printList();	// Expected Output : [1,2,3]
-	    
-	    list.insert(0, 4);	// Expected Output : [4,1,2,3]
-	    //list.insert(list.size(), 4);	// Expected Output : [1,2,3,4]
-	    //list.insert(1, 4);		// Expeted Output : [1,4,2,3]
-	    //list.insert(-1, 4);	// Expected Output : Error, IndexOutOfBoundsException
-	    //list.insert(4, 4);	// Expected Output : Error, IndexOutOfBoundsException
-	    list.printList();
-	    System.out.println("---------------------------------------\n\n");
+		System.out.println(list.removeFirst());	// Expected Output : C
 	}
 	
 	@Test
-	void appendTest() {
-		/* Start with the empty list */
-		LinkedList list = new LinkedList();
-		list.append(1);
-		list.append(2);
-		list.append(3);
+	void removeLastTest() {
+		LinkedList<String> list = new LinkedList<String>();
 		
-		list.printList();	// Expected Output : [1,2,3]	
+		list.addFirst("A");
+		list.addFirst("B");
+		list.addFirst("C");
+		
+		System.out.println(list.removeLast());	// Expected Output : A
 	}
 	
 	@Test
-	void deleteTest() {
-		LinkedList list = new LinkedList();
-		 
-        list.push(7);
-        list.push(1);
-        list.push(3);
-        list.push(2);
- 
-        System.out.println("before Linked List :");
-        list.printList();	// Expected Output : [2,3,1,7]
- 
-        list.deleteKeyNode(1); // 값이 1인 노드 삭제
- 
-        System.out.println("\nafter Linked List :");
-        list.printList(); // Expected Output : [2,3,7]
+	void removeTest() {
+		LinkedList<String> list = new LinkedList<String>();
+		
+		list.addFirst("A");
+		list.addFirst("B");
+		list.addFirst("C");
+		
+		System.out.println(list.remove("B"));	// Expected Output : B
+		System.out.println(list);				// Expected Output : C A
 	}
 	
 	@Test
-	void deletePositionTest() {
-		LinkedList list = new LinkedList();
-		 
-		list.push(7);
-		list.push(1);
-		list.push(3);
-		list.push(2);
-		list.push(8);
-        
+	void containsTest() {
+		LinkedList<String> list = new LinkedList<String>();
+		
+		list.addFirst("A");
+		list.addFirst("B");
+		list.addFirst("C");
+		
+		System.out.println(list.contains("B"));	// Expected Output : true
+	}
 
-        System.out.println("before Linked List :");
-        list.printList();	// Expected Output : [8,2,3,1,7]
- 
-        list.deletePositionNode(4); // 4번째 노드 삭제 요청
- 
-        System.out.println("\nafter Linked List : ");
-        list.printList(); // Expected Output : [8,2,3,1]
-	}
 }
