d0e08b28916e9ea5d0ae3067c5789fbdd9c85815
yonghwankim.dev <yonghwankim.dev@gmail.com>
2021-12-24 13:35:17 +0900

    bst rotate

--- Tree/BST/Implement/boost/BinarySearchTree.java
+++ Tree/BST/Implement/boost/BinarySearchTree.java
@@ -82,6 +82,38 @@ public class BinarySearchTree<E> {
 		return contains(obj, node.left);
 	}
 	
+	public Node<E> leftRotate(Node<E> node)
+	{
+		Node<E> temp = node.right;
+		
+		node.right = temp.left;
+		temp.left = node;
+		
+		return temp;	
+	}
+	
+	public Node<E> rightRotate(Node<E> node)
+	{
+		Node<E> temp = node.left;
+		
+		node.left = temp.right;
+		temp.right = node;
+		
+		return temp;
+	}
+	
+	public Node<E> leftRightRotate(Node<E> node)
+	{
+		node.left = leftRotate(node.left);
+		return rightRotate(node);
+	}
+	
+	public Node<E> rightLeftRotate(Node<E> node)
+	{
+		node.right = rightRotate(node.right);
+		return leftRotate(node);
+	}
+	
 	public void inorderTraversal()
 	{		
 		inorderTraversal(this.root);
