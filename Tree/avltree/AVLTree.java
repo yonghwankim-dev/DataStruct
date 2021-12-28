95ae4c8df3e66b1695ac5f63d5018549abf86e34
yonghwankim.dev <yonghwankim.dev@gmail.com>
2021-12-28 17:18:31 +0900

    avl tree rebalance

--- Tree/avltree/AVLTree.java
+++ Tree/avltree/AVLTree.java
@@ -1,5 +1,6 @@
 package Tree.avltree;
 
+
 public class AVLTree<T> {
 	private Node<T> root;
 	int currentSize;
@@ -33,12 +34,12 @@ public class AVLTree<T> {
 		else
 		{
 			add(root, node);
-		}		
+		}
 	}
 	private void add(Node<T> root, Node<T> newNode)
 	{
 		// newNode의 data가 root의 data보다 작은 경우
-		if(((Comparable<T>)newNode).compareTo(root.data)<=0)
+		if(((Comparable<T>)newNode.data).compareTo(root.data)<=0)	
 		{
 			if(root.left==null)
 			{
@@ -69,7 +70,144 @@ public class AVLTree<T> {
 		checkBalance(newNode);
 	}
 
-	private void checkBalance(Node<T> newNode) {
-		return;
+	private void checkBalance(Node<T> node) {
+		// 높이 차이가 1 초과 혹은 -1 미만 (AVL 트리 규칙 위반)
+		if((height(node.left)-height(node.right) > 1) || 
+				(height(node.left)-height(node.right) < -1))
+		{
+			rebalance(node);
+		}
+		
+		// 부모 노드를 게속 확인해서 루트까지 올라갑니다.
+		if(node.parent==null)
+		{
+			return;
+		}
+		checkBalance(node.parent);
+	}
+	
+	private void rebalance(Node<T> node) {
+		if(height(node.left) - height(node.right) > 1)	// 왼쪽 자식 > 오른쪽 자식
+		{
+			
+			if(height(node.left.left) > height(node.left.right)) // 왼쪽 서브 트리 > 오른쪽 서브 트리
+			{
+				node = rightRotate(node);		// 우측 회전
+			}
+			else	// 왼쪽 서브 트리 < 오른쪽 서브 트리
+			{
+				node = leftRightRotate(node);	// 좌측-우측 회전
+			}
+		}
+		else	// 왼쪽 자식 < 오른쪽 자식
+		{
+			if(height(node.right.left) > height(node.right.right))	// 왼쪽 서브 트리 > 오른쪽 서브 트리
+			{
+				node = rightLeftRotate(node);	// 우측-좌측 회전
+			}
+			else	// 왼쪽 서브 트리 < 오른쪽 서브 트리
+			{
+				node = leftRotate(node);	// 좌측 회전
+			}
+		}
+		
+		// 루트로 올때까지 반복
+		if(node.parent==null)
+		{
+			root = node;
+		}
+	}
+
+	private int height(Node<T> node)
+	{
+		if(node==null)
+		{
+			return 0;
+		}
+		else
+		{
+			return Math.max(height(node.left), height(node.right))+1;
+		}	
+	}
+	
+	// root 노드를 기준으로 왼쪽 회전
+	private Node<T> leftRotate(Node<T> root)
+	{
+		Node<T> temp = root.right;
+		
+		root.right = temp.left;
+		temp.left = root;
+		
+		temp.parent = root.parent;
+		
+		if(root.parent!=null && root.parent.left==root)
+		{
+			root.parent.left = temp;
+		}
+		else if(root.parent!=null && root.parent.right==root)
+		{
+			root.parent.right = temp;
+		}
+		
+		root.parent = temp;
+		return temp;	
+	}
+	
+	// root 노드를 기준으로 오른쪽 회전
+	private Node<T> rightRotate(Node<T> root)
+	{
+		Node<T> temp = root.left;
+		
+		root.left = temp.right;
+		temp.right = root;
+		
+		temp.parent = root.parent;
+		
+		if(root.parent!=null && root.parent.left==root)
+		{
+			root.parent.left = temp;
+		}
+		else if(root.parent!=null && root.parent.right==root)
+		{
+			root.parent.right = temp;
+		}
+		
+		root.parent = temp;
+		
+		return temp;
+	}
+
+	// root 노드를 기준으로 왼쪽-오른쪽 회전
+	private Node<T> leftRightRotate(Node<T> root)
+	{
+		root.left = leftRotate(root.left);
+		
+		return rightRotate(root);
+	}
+	
+	// root 노드를 기준으로 오른쪽-왼쪽 회전
+	private Node<T> rightLeftRotate(Node<T> root)
+	{
+		root.right = rightRotate(root.right);
+		return leftRotate(root);
+	}
+	
+	// 중위 순회
+	public void inorderTraversal()
+	{		
+		inorderTraversal(this.root);
+		System.out.println();
+	}
+	
+	private void inorderTraversal(Node<T> root)
+	{
+		if(root==null)
+		{
+			return;
+		}
+		
+		inorderTraversal(root.left);
+		System.out.print(root.data + " ");
+		inorderTraversal(root.right);
 	}
 }
---
+++ Tree/avltree/AVLTreeTest.java
@@ -0,0 +1,22 @@
+package Tree.avltree;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AVLTreeTest {
+
+	@Test
+	void addTest() {
+		AVLTree<Integer> avlTree = new AVLTree<Integer>();
+		int[] inputs = {43,18,22,9,21,6,8,20,63,50,62,51};
+		
+		for(int i=0; i<inputs.length;i++)
+		{
+			System.out.print(inputs[i] + " add after : ");
+			avlTree.add(inputs[i]);
+			avlTree.inorderTraversal();
+		}	
+	}
+
+}
